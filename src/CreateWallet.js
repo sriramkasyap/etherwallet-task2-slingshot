045840ba22e04b3e679c7b25dc4537770bbc0f49
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-11-23 19:42:08 +0530

    create wallet functionality deon

--- src/CreateWallet.js
+++ src/CreateWallet.js
@@ -1,10 +1,52 @@
-import React, { useContext } from "react";
+import { Wallet } from "@ethersproject/wallet";
+import { AES } from "crypto-js";
+import React, { useContext, useState } from "react";
 import { ScreenContext } from "./App";
 
 const CreateWallet = () => {
-  const screenContext = useContext(ScreenContext);
-  console.log(screenContext);
-  return <div></div>;
+  const { setPassword, setScreen } = useContext(ScreenContext);
+
+  const [passwordKey, setPasswordKey] = useState();
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    let wallet = Wallet.createRandom();
+
+    console.log(wallet.privateKey);
+
+    let encrypted = AES.encrypt(wallet.privateKey, passwordKey).toString();
+    console.log(encrypted);
+    localStorage.setItem("sling_saved_pkey", encrypted);
+    setPassword(passwordKey);
+    setScreen("wallet");
+  };
+  return (
+    <div className="create-wallet-screen">
+      <h2>Create a new Wallet</h2>
+
+      <form onSubmit={handleFormSubmit}>
+        <div className="input-holder">
+          <input
+            type="passwordKey"
+            placeholder="Create a Password"
+            className="form-control"
+            name="passwordKey"
+            value={passwordKey}
+            required={true}
+            onChange={(e) => setPasswordKey(e.target.value)}
+          />
+        </div>
+        <p className="helper-text">
+          This password will be used to encrypt your keys. Please save it in a
+          safe place
+        </p>
+        <button className="submit-button" type="submit">
+          Create Wallet
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateWallet;
