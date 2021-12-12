fc3d9c829214f30d0c230a0368d9c48ca7873ce0
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-12-12 21:37:51 +0530

    showing NFTs

--- src/singleWallet/WalletScreen.js
+++ src/singleWallet/WalletScreen.js
@@ -1,5 +1,6 @@
-import { Box, Divider, Flex, Heading, Text } from "@chakra-ui/layout";
+import { Box, Divider, Flex, Heading, Text, Stack } from "@chakra-ui/layout";
 import {
+  Link,
   Button,
   Input,
   InputGroup,
@@ -10,14 +11,16 @@ import {
   Th,
   Thead,
   Tr,
+  Tag,
 } from "@chakra-ui/react";
 import { formatEther } from "@ethersproject/units";
 import { AES, enc, lib } from "crypto-js";
 import { ethers, providers } from "ethers";
 import { getAddress } from "ethers/lib/utils";
-import React, { useContext, useEffect, useState } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import { ScreenContext } from "../App";
 import TransferETH from "./TransferETH";
+import ERC721_ABI from "../ERC721_ABI";
 
 const ERC20_ABI = [
   // Read-Only Functions
@@ -37,11 +40,13 @@ const WalletScreen = () => {
   const [wallet, setWallet] = useState();
   const [error, setError] = useState();
   const [importing, setImporting] = useState();
-  const [tokenAddress, setTokenAddress] = useState();
+  const [tokenAddress, setTokenAddress] = useState("");
+  const [erc721tokenAddress, setERC721TokenAddress] = useState("");
   const { password, setScreen, currentWalletAddress } =
     useContext(ScreenContext);
   const [balance, setBalance] = useState(null);
   const [supportedTokens, setSupportedTokens] = useState([]);
+  const [supportedNFTs, setSupportedNFTs] = useState([]);
 
   useEffect(() => {
     if (
@@ -90,7 +95,6 @@ const WalletScreen = () => {
           return erc20Contract
             .balanceOf(wallet.address)
             .then((curWalletBalance) => {
-              console.log(curWalletBalance);
               if (curWalletBalance.gt(0)) {
                 tokensWithBalance.push({
                   ...token,
@@ -101,12 +105,48 @@ const WalletScreen = () => {
         })
       );
       setSupportedTokens(tokensWithBalance);
-      setSupportedTokens(tokensWithBalance);
+    };
+
+    const getERC721Balances = async () => {
+      setImporting(true);
+      let provider = new providers.JsonRpcProvider(
+        "https://rinkeby.infura.io/v3/a610e824d6bc4bef94728de6b76a098f"
+      );
+
+      let tokens = JSON.parse(localStorage.getItem("SupportedNFTs") || "[]");
+      let tokensWithBalance = [];
+      await Promise.all(
+        tokens.map(async (token) => {
+          let erc721contract = new ethers.Contract(
+            token.address,
+            ERC721_ABI,
+            provider
+          );
+          return erc721contract
+            .balanceOf(wallet.address)
+            .then(async (curWalletBalance) => {
+              if (curWalletBalance.gt(0)) {
+                let nftokens = await getNFTList(
+                  token.address,
+                  curWalletBalance
+                );
+                tokensWithBalance.push({
+                  ...token,
+                  balance: curWalletBalance,
+                  nftokens,
+                });
+              }
+            });
+        })
+      );
+      setSupportedNFTs(tokensWithBalance);
+      setImporting(false);
     };
 
     if (wallet) {
       setwalletBalance();
       getERC20Balances();
+      getERC721Balances();
     }
   }, [wallet]);
 
@@ -127,7 +167,6 @@ const WalletScreen = () => {
         return erc20Contract
           .balanceOf(wallet.address)
           .then((curWalletBalance) => {
-            console.log(curWalletBalance);
             if (curWalletBalance.gt(0)) {
               tokensWithBalance.push({
                 ...token,
@@ -138,6 +177,39 @@ const WalletScreen = () => {
       })
     );
     setSupportedTokens(tokensWithBalance);
+    setImporting(false);
+  };
+
+  const refreshERC721Balances = async () => {
+    let tokens = JSON.parse(localStorage.getItem("SupportedNFTs") || "[]");
+    let tokensWithBalance = [];
+    let provider = new providers.JsonRpcProvider(
+      "https://rinkeby.infura.io/v3/a610e824d6bc4bef94728de6b76a098f"
+    );
+
+    await Promise.all(
+      tokens.map(async (token) => {
+        let erc721Contract = new ethers.Contract(
+          token.address,
+          ERC721_ABI,
+          provider
+        );
+        return erc721Contract
+          .balanceOf(wallet.address)
+          .then(async (curWalletBalance) => {
+            if (curWalletBalance.gt(0)) {
+              let nftokens = await getNFTList(token.address, curWalletBalance);
+              tokensWithBalance.push({
+                ...token,
+                balance: curWalletBalance,
+                nftokens,
+              });
+            }
+          });
+      })
+    );
+    setSupportedNFTs(tokensWithBalance);
+    setImporting(false);
   };
 
   const handleTokenImport = async (e) => {
@@ -167,12 +239,60 @@ const WalletScreen = () => {
       }
 
       await localStorage.setItem("SupportedTokens", JSON.stringify(tokens));
-      setImporting(false);
       refreshERC20Balances();
     } catch (error) {
       console.error(error);
       setError(error.message);
     }
+  };
+
+  const handleNFTImport = async (e) => {
+    e.preventDefault();
+    setError("");
+    setImporting(true);
+    try {
+      let address = getAddress(erc721tokenAddress);
+      let provider = new providers.JsonRpcProvider(
+        "https://rinkeby.infura.io/v3/a610e824d6bc4bef94728de6b76a098f"
+      );
+      let erc721Contract = new ethers.Contract(address, ERC721_ABI, provider);
+      let supToken = {
+        symbol: await erc721Contract.symbol(),
+        name: await erc721Contract.name(),
+        address: erc721Contract.address,
+      };
+
+      let tokens = JSON.parse(localStorage.getItem("SupportedNFTs") || "[]");
+
+      let exists = tokens.filter(
+        (token) => token.address === erc721Contract.address
+      );
+      if (!(exists.length > 0)) {
+        tokens.push(supToken);
+      }
+
+      await localStorage.setItem("SupportedNFTs", JSON.stringify(tokens));
+      refreshERC721Balances();
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    }
+  };
+
+  const getNFTList = async (address, balance) => {
+    let provider = new providers.JsonRpcProvider(
+      "https://rinkeby.infura.io/v3/a610e824d6bc4bef94728de6b76a098f"
+    );
+    let erc721Contract = new ethers.Contract(address, ERC721_ABI, provider);
+    let nftokens = [];
+    for (let i = 0; i < balance; i++) {
+      let nfttoken = await erc721Contract.tokenOfOwnerByIndex(
+        wallet.address,
+        i
+      );
+      nftokens.push(nfttoken.toString());
+    }
+    return nftokens;
   };
 
   return (
@@ -222,21 +342,23 @@ const WalletScreen = () => {
                         {balance !== null ? formatEther(balance) : "---"} ETH
                       </Td>
                     </Tr>
-                    {supportedTokens.map((token) => {
-                      return (
-                        <Tr>
-                          <Td>{token.symbol}</Td>
-                          <Td>{token.name}</Td>
-                          <Td whiteSpace={"nowrap"} isNumeric>
-                            {token.balance !== null &&
-                            token.balance !== undefined
-                              ? formatEther(token.balance)
-                              : "---"}{" "}
-                            {token.symbol}
-                          </Td>
-                        </Tr>
-                      );
-                    })}
+                    {supportedTokens.length > 0
+                      ? supportedTokens.map((token, t) => {
+                          return (
+                            <Tr key={t}>
+                              <Td>{token.symbol}</Td>
+                              <Td>{token.name}</Td>
+                              <Td whiteSpace={"nowrap"} isNumeric>
+                                {token.balance !== null &&
+                                token.balance !== undefined
+                                  ? formatEther(token.balance)
+                                  : "---"}{" "}
+                                {token.symbol}
+                              </Td>
+                            </Tr>
+                          );
+                        })
+                      : "Loading..."}
                   </Tbody>
                 </Table>
               </Box>
@@ -258,6 +380,85 @@ const WalletScreen = () => {
                       h="1.75rem"
                       size="sm"
                       onClick={handleTokenImport}
+                    >
+                      {importing ? "..." : "Import"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </Box>
+            </Flex>
+            <Divider m="20px 0" />
+            <Flex mt={5} width={"100%"} pb={5}>
+              <Box flex={3} borderRight={"1px solid #eee"} pb={7} pr={7}>
+                <Heading mb={5} size={"md"}>
+                  NFTs Owned
+                </Heading>
+                <Stack>
+                  {!importing && supportedNFTs.length === 0 ? (
+                    <Text>No NFTs Found</Text>
+                  ) : null}
+                  {supportedNFTs.map((nft, n) => {
+                    return (
+                      <Fragment key={n}>
+                        <Flex pt={3} pb={3} justifyContent={"space-between"}>
+                          <Text size={"xs"}>
+                            <strong>Collection</strong>: {nft.symbol} (
+                            {nft.name})
+                          </Text>
+                          <Text size={"xs"}>
+                            <strong>You Own: </strong>
+                            {ethers.utils.formatUnits(nft.balance, "wei")} NFTs
+                          </Text>
+                        </Flex>
+                        <Flex alignItems={"center"}>
+                          <Heading size={"xs"} maxW={"25%"}>
+                            List of {nft.symbol}s you own:
+                          </Heading>
+                          <Box ml={5} justifySelf={"flex-start"}>
+                            {nft.nftokens.map((n) => (
+                              <Tag>{n}</Tag>
+                            ))}
+                          </Box>
+                          <Link
+                            target={"_blank"}
+                            justifySelf={"flex-end"}
+                            ml="auto"
+                            href={`https://rinkeby.etherscan.io/address/${nft.address}`}
+                          >
+                            <Text
+                              size={"xs"}
+                              textDecor={"underline"}
+                              fontSize={10}
+                            >
+                              View on Etherscan
+                            </Text>
+                          </Link>
+                        </Flex>
+
+                        <Divider m="20px 0" />
+                      </Fragment>
+                    );
+                  })}
+                </Stack>
+              </Box>
+              <Box flex={2} pl={7}>
+                <Heading mb={5} size={"sm"}>
+                  Import NFTs
+                </Heading>
+                <InputGroup size="md">
+                  <Input
+                    pr="4.5rem"
+                    type="text"
+                    placeholder="NFT Contract Address"
+                    value={erc721tokenAddress}
+                    onChange={(e) => setERC721TokenAddress(e.target.value)}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      disabled={importing}
+                      h="1.75rem"
+                      size="sm"
+                      onClick={handleNFTImport}
                     >
                       {importing ? "..." : "Import"}
                     </Button>
